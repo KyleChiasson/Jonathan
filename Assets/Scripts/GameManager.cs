59ec122df42313990de7464ece43ce1c4366cf25
KyleChiasson <89480424+KyleChiasson@users.noreply.github.com>
2022-10-05 09:47:16 -0400

    Finished For Demo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,15 +11,13 @@ public class GameManager : MonoBehaviour
         if (instance == null)
             instance = this;
         else Destroy(this);
+        Application.targetFrameRate = 60;
     }
-    private void Start()
-    {
-        AdvanceScene(startingScene);
-    }
+    //Text stuff
     public SOScene startingScene;
-    public TMP_Text character1Text;
+    public SOScene endingScene;
+    public TMP_Text characterText;
     public TMP_Text character1NameText;
-    public TMP_Text character2Text;
     public TMP_Text character2NameText;
     public Image character1Image;
     public Image character2Image;
@@ -31,23 +29,64 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
-        if (!choosing && Input.GetMouseButtonDown(0))
+        if (playing && !choosing && Input.GetMouseButtonDown(0))
         {
             AdvanceDialogue();
         }
     }
 
+    //Sleep Stuff
+    private bool asleep = true;
+    private bool changingState = false;
+    public Material sleepMat;
+    private float sleepAmt = 0;
+    private delegate void VOID();
+    private IEnumerator ChangeSleepState(VOID callback = null)
+    {
+        changingState = true;
+        while (asleep ? (sleepAmt < .3f) : (sleepAmt > 0))
+        {
+            yield return new WaitForSeconds(.1f);
+            sleepAmt += asleep ? .01f : -.01f;
+            sleepMat.SetFloat("_Float", sleepAmt);
+        }
+        asleep = !asleep;
+        changingState = false;
+        if(callback != null)
+            callback();
+    }
+
+    //Dialogue Stuff
+    private bool playing = false;
     private SOScene currentScene;
     private SOConversation currentConversation;
     private int currentDialogue;
-    private int stress = 25;
+    private int stress = 25; //goes between 0-60
     private bool choosing = false;
     private void AdvanceDialogue()
     {
+        if (currentDialogue == currentConversation.speech.Length - 1 && currentConversation.choices.Length == 0)
+        {
+            AdvanceScene(currentScene.nextScenes[0]);
+            return;
+        }
         currentDialogue++;
-        character1Image.sprite = currentConversation.speech[currentDialogue].character.art;
-        character1Text.text = currentConversation.speech[currentDialogue].line;
-        if (currentDialogue == currentConversation.speech.Length - 1)
+        if (currentConversation.speech[currentDialogue].character.character_name == "You")
+        {
+            character1NameText.text = currentConversation.speech[currentDialogue].character.character_name;
+            character1Image.sprite = currentConversation.speech[currentDialogue].character.art;
+            characterText.alignment = TextAlignmentOptions.MidlineLeft;
+        }
+        else
+        {
+            character2NameText.text = currentConversation.speech[currentDialogue].character.character_name;
+            character2Image.sprite = currentConversation.speech[currentDialogue].character.art;
+            if (currentConversation.speech[currentDialogue].character.character_name == "")
+                characterText.alignment = TextAlignmentOptions.Midline;
+            else characterText.alignment = TextAlignmentOptions.MidlineRight;
+        }
+        characterText.text = currentConversation.speech[currentDialogue].line;
+        if (currentConversation.choices.Length != 0 && currentDialogue == currentConversation.speech.Length - 1)
         {
             choosing = true;
             button1.interactable = true;
@@ -58,6 +97,7 @@ public class GameManager : MonoBehaviour
     }
     public void MakeChoice(int choice)
     {
+        choosing = false;
         button1.interactable = false;
         button2.interactable = false;
         choice1.text = "";
@@ -78,10 +118,50 @@ public class GameManager : MonoBehaviour
 
     private void AdvanceScene(SOScene newScene)
     {
+        character1NameText.text = "";
+        character2NameText.text = "";
+        character1Image.sprite = blankSprite;
+        character2Image.sprite = blankSprite;
         currentScene = newScene;
-        currentConversation = newScene.startingConversation;
+        if(currentScene == endingScene)
+        {
+            EndGame();
+            return;
+        }
+        currentConversation = currentScene.startingConversation;
         currentDialogue = -1;
-        //backGroundImage.sprite = newScene.background;
+        backGroundImage.sprite = newScene.background;
         AdvanceDialogue();
+    }
+
+    //Start and end
+    public Sprite blankSprite;
+    public void StartGame()
+    {
+        stress = 25;
+        sleepMat.SetFloat("_Float", sleepAmt);
+        StartCoroutine(ChangeSleepState());
+        AdvanceScene(startingScene);
+        playing = true;
+    }
+    private void EndGame()
+    {
+        playing = false;
+        StartCoroutine(ChangeSleepState(EndCallback));
+    }
+    public GameObject gameUI;
+    public GameObject endUI;
+    public TMP_Text stressText;
+    public GameObject wonText;
+    public GameObject replayButton;
+    public GameObject mainMenuButton;
+    private void EndCallback()
+    {
+        endUI.SetActive(true);
+        gameUI.SetActive(false);
+        stressText.text = $"Stress: {Mathf.RoundToInt(100f * stress / 60f)}%";
+        wonText.SetActive((stress == 60 || stress == 0) ? true : false);
+        replayButton.SetActive((stress == 60 || stress == 0) ? false : true);
+        mainMenuButton.SetActive((stress == 60 || stress == 0) ? true : false);
     }
 }
--- Assets/Scripts/ScriptableObjects/SOCharacter.cs
+++ Assets/Scripts/ScriptableObjects/SOCharacter.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Character", menuName = "SO/Character")]
 public class SOCharacter : ScriptableObject
 {
+    public string character_name;
     public Sprite art;
 }
